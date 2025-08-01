f336d928f5c8b6f599b8c4522e95a32e0c997e37
Mauro <ivanmauricio.o.parrado@gmail.com>
2025-07-31 19:00:03 -0500

    se corrigio con licencias

--- app/Http/Controllers/Api/V1/EntrenosController.php
+++ app/Http/Controllers/Api/V1/EntrenosController.php
@@ -3,27 +3,12 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
-
-use App\Services\EntrenoService;
+use App\LogicaNegocio\Entrenos\EntrenosLogicaNegocio;
 
 class EntrenosController extends Controller
 {
-    protected $reglaCrear = [
-        'cliente_id'        => 'nullable|integer|exists:clientes,id',
-        'tipo'              => 'required|in:individual,mensualidad,equipo',
-        'pago_id'           => 'nullable|integer|exists:pagos,id',
-        'liga_id'           => 'required|integer|exists:ligas,id'
-    ];
-
-    protected $reglaActualizar = [
-        'cliente_id'        => 'sometimes|nullable|integer|exists:clientes,id',
-        'tipo'              => 'sometimes|required|in:individual,mensualidad,equipo',
-        'pago_id'           => 'sometimes|nullable|integer|exists:pagos,id',
-        'liga_id'           => 'sometimes|required|integer|exists:ligas,id'
-    ];
-
     public function __construct()
     {
-        parent::__construct(new EntrenoService(), $this->reglaCrear, $this->reglaActualizar);
+        parent::__construct(new EntrenosService());
     }
 }
